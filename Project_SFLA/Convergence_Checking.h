1dfddeb92e8d081681821f3cba2f74259d012ca0
kimia sadeghi <kimiasq83@gmail.com>
2025-05-26 04:05:07 -0700

    Final code commit

---
+++ Project_SFLA/Convergence_Checking.h
@@ -0,0 +1,10 @@
+#include <systemc.h>
+#include <vector>
+
+SC_MODULE(Convergence_Checking) {
+    void check_convergence();
+
+    SC_CTOR(Convergence_Checking) {
+        SC_THREAD(check_convergence);
+    }
+};
